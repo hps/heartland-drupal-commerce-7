bc3b15fcfbe08fac274ad1f1fefab6b5391c449a
mayur-vachchewar <MVachchewar@tsys.com>
2024-08-26 16:26:28 +0530

    updated secure submit to globalpayment js

--- commerce_securesubmit/commerce_securesubmit.js
+++ commerce_securesubmit/commerce_securesubmit.js
@@ -8,29 +8,88 @@
     attach: function (context, settings) {
       if (settings.securesubmit.fetched == null) {
         settings.securesubmit.fetched = true;
+        // Configure Globalpayment JS
+        GlobalPayments.configure({
+          publicApiKey: settings.securesubmit.publicKey
+        });
 
-        var createToken = function (cardFieldMap) {
-          hps.tokenize({
-            data: {
-              public_key: settings.securesubmit.publicKey,
-              number: $('[id^=' + cardFieldMap.number +']').val(),
-              cvc: $('[id^=' + cardFieldMap.cvc +']').val(),
-              exp_month: $('[id^=' + cardFieldMap.exp_month +']').val(),
-              exp_year: $('[id^=' + cardFieldMap.exp_year +']').val()
+        const hps = GlobalPayments.ui.form({
+          fields: {
+            "card-number": {
+              placeholder: "•••• •••• •••• ••••",
+              target: "#edit-commerce-payment-payment-details-credit-card-number"
             },
-            success: function(response) {
-              secureSubmitResponseHandler(response);
+            "card-expiration": {
+              placeholder: "MM / YYYY",
+              target: "#edit-commerce-payment-payment-details-credit-card-expiry-date"
             },
-            error: function(response) {
-              secureSubmitResponseHandler(response);
+            "card-cvv": {
+              placeholder: "•••",
+              target: "#edit-commerce-payment-payment-details-credit-card-code"
             }
-          });
-        };
+          }
+        });
+
+        hps.ready(() => {
+          console.log("Registration of all credit card fields occurred");
+        });
+
+        hps.on("token-success", (resp) => {
+          secureSubmitResponseHandler(resp);
+        });
+
+        hps.on("token-error", (resp) => {
+          secureSubmitResponseHandler(resp);
+          return true;
+        });
+
+        // hps.on("token-success", function(response) {
+        //   secureSubmitResponseHandler(response);
+        // });
+        //
+        // hps.on("token-error", function(response) {
+        //   secureSubmitResponseHandler(response);
+        // });
+
+        var triggerSubmit = function () {
+          // manually include iframe submit button
+          const fields = ['submit'];
+          const target = hps.frames['card-number'];
+
+          for (const type in hps.frames) {
+            if (hps.frames.hasOwnProperty(type)) {
+              fields.push(type);
+            }
+          }
+
+          for (const type in hps.frames) {
+            if (!hps.frames.hasOwnProperty(type)) {
+              continue;
+            }
+
+            const frame = hps.frames[type];
+
+            if (!frame) {
+              continue;
+            }
+
+            GlobalPayments.internal.postMessage.post({
+              data: {
+                fields: fields,
+                target: target.id
+              },
+              id: frame.id,
+              type: 'ui:iframe-field:request-data'
+            }, frame.id);
+          }
+        }
 
         function secureSubmitResponseHandler(response) {
-          if (response.message) {
+          var error_message = response?.error?.message ?? response?.reasons[0]?.message;
+
+          if (error_message) {
             // Show the errors on the form.
-            $('div.payment-errors').html($("<div class='messages messages--error error'></div>").html(response.message));
+            $('div.payment-errors').html($("<div class='messages messages--error error'></div>").html(error_message));
 
             if (window.onError) {
               window.onError(form$);
@@ -39,19 +98,17 @@
             $('.form-submit').removeAttr("disabled");
             $('.form-submit').prop('disabled', null);
           } else {
-            var token = response.token_value;
+            var token = response.paymentReference ? response.paymentReference : response.token_value;
             $("#edit-continue").closest("form").append("<input type='hidden' name='securesubmitToken' value='" + token + "'/>");
             $("#edit-continue").closest("form").get(0).submit($("#edit-continue").closest("form"));
           }
         }
 
+
         $('body').delegate('#edit-continue', 'click', function(event) {
           // ensure we are selected
           if ($("input[value*='commerce_securesubmit|']").is(':checked')) {
             $(this).addClass('auth-processing');
-
-            // Prevent the form from submitting with the default action.
-            event.preventDefault();
 
             // Show progress animated gif (needed for submitting after first error).
             $('.checkout-processing').show();
@@ -59,16 +116,19 @@
             // Disable the submit button to prevent repeated clicks.
             $('.form-submit').attr("disabled", "disabled");
 
+            // Prevent the form from submitting with the default action.
+            event.preventDefault();
+
             var cardFields = {
-              number: 'edit-commerce-payment-payment-details-credit-card-number',
-              cvc: 'edit-commerce-payment-payment-details-credit-card-code',
               exp_month: 'edit-commerce-payment-payment-details-credit-card-exp-month',
               exp_year: 'edit-commerce-payment-payment-details-credit-card-exp-year',
-              name: 'edit-commerce-payment-payment-details-credit-card-owner'
             };
 
-            createToken(cardFields);
-
+            $('#edit-commerce-payment-payment-details-credit-card-expiry-date').val(
+              $('[id^=' + cardFields.exp_month +']').val() +'/' +
+              $('[id^=' + cardFields.exp_year +']').val()
+            );
+            triggerSubmit();
             return false;
           }
         });
